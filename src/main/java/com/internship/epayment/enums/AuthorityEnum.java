5cb2c334fd31a742dcd78aa6ab96273ff1698f0e
Daniel.Raduna <raduna.daniel@trencadis.ro>
2021-07-07 11:49:45 +0100

    feat(security): basic auth authorities

--- src/main/java/com/internship/epayment/enums/AuthorityEnum.java
+++ src/main/java/com/internship/epayment/enums/AuthorityEnum.java
@@ -1,7 +1,8 @@
 package com.internship.epayment.enums;
 
 public enum AuthorityEnum {
-    EPAY_ADMIN("EPAY_ADMIN");
+    EPAY_ADMIN("EPAY_ADMIN"),
+    EPAY_USER("EPAY_USER");
 
     private final String code;
 
