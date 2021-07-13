0e266ca5d0fe8d64f6b550bde5bf8176c3e08326
Luiza.Patroescu <luiza.patroescu@trencadis.ro>
2021-07-12 18:54:46 -0700

    feat(angular): added "crd" frontend for product

---
+++ src/main/webapp/epayclient/src/app/components/product-delete/product-delete.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from "../../entities/product";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {Observable} from "rxjs";
+import {HttpResponse} from "@angular/common/http";
+import {ToastrService} from "ngx-toastr";
+import {ProductService} from "../../services/product.service";
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  inputProduct?: Product;
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private productService: ProductService,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  close(): void {
+    this.activeModal.close(false);
+  }
+
+  delete(): void {
+    this.subscribeToSaveResponse(this.productService.deleteProduct(this.inputProduct!));
+  }
+
+  private subscribeToSaveResponse(result: Observable<HttpResponse<Product>>): void {
+    result.subscribe(
+      () => this.onDeleteSuccess(),
+      () => this.onDeleteError()
+    );
+  }
+
+  private onDeleteSuccess(): void {
+    this.activeModal.close(true);
+    this.toastr.success('Product deleted!', 'Success!');
+  }
+
+  private onDeleteError(): void {
+    this.toastr.error('Error deleting product!', 'Error!');
+  }
+}
