c4701182a9408a05354bc75ca502cb0053ba524d
andreea.cuzmin <andreea.cuzmin@trencadis.ro>
2021-07-29 10:15:28 +0300

    feat(map): harta widget cauta dupa adresa -> add pin

--- src/main/webapp/epayclient/src/app/components/user-form/user-form.component.ts
+++ src/main/webapp/epayclient/src/app/components/user-form/user-form.component.ts
@@ -65,7 +65,7 @@ export class UserFormComponent implements OnInit {
         this.ngZone.run(() => {
           //get the place result
           let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-          console.log("branzaaaaaaaaaaaaaaaaa",place );
+     //     console.log("branzaaaaaaaaaaaaaaaaa",place );
 
           //verify result
           if (place.geometry === undefined || place.geometry === null) {
@@ -220,13 +220,6 @@ export class UserFormComponent implements OnInit {
     });
   }
 
-  // markerDragEnd2($event: MouseEvent) {
-  //   console.log($event);
-  //   this.latitude = $event.coords.lat;
-  //   this.longitude = $event.coords.lng;
-  //   this.getAddress(this.latitude, this.longitude);
-  // }
-
   getAddress(latitude: number, longitude: number) {
     this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results: { formatted_address: string | undefined; }[], status: string) => {
       console.log(results);
@@ -246,75 +239,6 @@ export class UserFormComponent implements OnInit {
   }
 
   markers: marker[] = [];
-//
-//   // This example adds a search box to a map, using the Google Place Autocomplete
-// // feature. People can enter geographical searches. The search box will return a
-// // pick list containing a mix of places and predicted search terms.
-// // This example requires the Places library. Include the libraries=places
-// // parameter when you first load the API. For example:
-// // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-//   initAutocomplete() {
-//     const map = new google.maps.Map(document.getElementById("map")!, {
-//       center: { lat: -33.8688, lng: 151.2195 },
-//       zoom: 12,
-//       mapTypeId: "roadmap",
-//     });
-//     // Create the search box and link it to the UI element.
-//     const input = document.getElementById("pac-input")!;
-//     const searchBox = new google.maps.places.SearchBox(input);
-//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-//     // Bias the SearchBox results towards current map's viewport.
-//     map.addListener("bounds_changed", () => {
-//       searchBox.setBounds(map.getBounds());
-//     });
-//     let markers: google.maps.Marker[] = [];
-//     // Listen for the event fired when the user selects a prediction and retrieve
-//     // more details for that place.
-//     searchBox.addListener("places_changed", () => {
-//       const places = searchBox.getPlaces();
-//
-//       if (places.length == 0) {
-//         return;
-//       }
-//       // Clear out the old markers.
-//       markers.forEach((marker) => {
-//         marker.setMap(null);
-//       });
-//       markers = [];
-//       // For each place, get the icon, name and location.
-//       const bounds = new google.maps.LatLngBounds();
-//       places.forEach((place) => {
-//         if (!place.geometry || !place.geometry.location) {
-//           console.log("Returned place contains no geometry");
-//           return;
-//         }
-//         const icon = {
-//           url: place.icon,
-//           size: new google.maps.Size(71, 71),
-//           origin: new google.maps.Point(0, 0),
-//           anchor: new google.maps.Point(17, 34),
-//           scaledSize: new google.maps.Size(25, 25),
-//         };
-//         // Create a marker for each place.
-//         markers.push(
-//           new google.maps.Marker({
-//             map,
-//             icon,
-//             title: place.name,
-//             position: place.geometry.location,
-//           })
-//         );
-//
-//         if (place.geometry.viewport) {
-//           // Only geocodes have viewport.
-//           bounds.union(place.geometry.viewport);
-//         } else {
-//           bounds.extend(place.geometry.location);
-//         }
-//       });
-//       map.fitBounds(bounds);
-//     });
-//   }
  }
 
 interface marker {
